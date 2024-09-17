81d7bba92c2945d875fa6d647a50ad537479a65a
gutsszz <talhawqqas14@gmail.com>
2024-09-17 17:38:02 +0500

    store change

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_tCpObdQvEjVJTJRrjjn44SeOrbdbpdU",
-  authDomain: "proj-64fca.firebaseapp.com",
-  projectId: "proj-64fca",
-  storageBucket: "proj-64fca.appspot.com",
-  messagingSenderId: "481368871573",
-  appId: "1:481368871573:web:d7492eb63b209424f6819c",
-  measurementId: "G-SGW24CSCGP"
+  apiKey: "AIzaSyBPshT4yjPpghRgSw8DmVrrNn52i0hF4Rw",
+  authDomain: "project1-8709d.firebaseapp.com",
+  databaseURL: "https://project1-8709d-default-rtdb.firebaseio.com",
+  projectId: "project1-8709d",
+  storageBucket: "project1-8709d.appspot.com",
+  messagingSenderId: "930208080927",
+  appId: "1:930208080927:web:3009ff4c1abc62c797a12a",
+  measurementId: "G-FBMDQQF66B"
 };
 
 
